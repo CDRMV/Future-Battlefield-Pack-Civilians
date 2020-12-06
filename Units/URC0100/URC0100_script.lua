d8d777bb13c7625a5375e77377e33fa2d900d876
CDRMV <46399086+CDRMV@users.noreply.github.com>
2020-12-06 13:21:40 +0100

    New Submod, which adds the new Civilian Engineer and make Civilian Structures buildable by it. New Civilian Buildings will be added later

---
+++ Units/URC0100/URC0100_script.lua
@@ -0,0 +1,15 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/URL0105/URL0105_script.lua
+#**  Author(s):  David Tomandl
+#**
+#**  Summary  :  Cybran T1 Engineer Script
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+local CConstructionUnit = import('/lua/cybranunits.lua').CConstructionUnit
+
+URC0100 = Class(CConstructionUnit) {}
+
+TypeClass = URC0100
---
+++ mod_info.lua
@@ -0,0 +1,14 @@
+name = "(F.B.P.) Future Battlefield Pack: Civilians"
+version = 1.0
+copyright = "Copyright © 2020, CDRMV (aka CDR Deadalus)"
+author = "CDRMV (aka CDR Deadalus)"
+description = "'Future Battlefield Pack: Civilians' is a Submod of the F.B.P. Modproject. It adds fully new Civilian Buildings and make the exiting ones buildable by the new Civilian Engineer."
+url = "http://www.moddb.com/mods/fbp-future-battlefield-pack"
+
+uid = "5t3edt-btz6-9437-h6ui-967gt56fa9"
+exclusive = false
+ui_only = false
+selectable = true
+requires = {}
+icon = "/mods/Future Battlefield Pack Civilians/Icon.png" 
+before = { }
